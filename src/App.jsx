798b4ec0a35fa294a3bfb8910b42a41d16d53128
Emmanuel Sunday <omegasantiago444@gmail.com>
2023-05-21 20:49:47 +0100

    fix: change names

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,26 @@
 import React from 'react'
 import GoogleLogo from './components/GoogleLogo'
 import SearchForm from './components/SearchForm'
-import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 
 
 function App() {
   return (
+  <>
     <div className='google-wrapper'>
-      <nav>
+      
         <Navbar />
-      </nav>
-      <header>
+      
+      
         <GoogleLogo />
-      </header>
-      <main>
+      
+      
         <SearchForm />
-      </main>
-      <footer className='footer'>
-        <Footer />
-      </footer>
-
+      
     </div>
+    
+   </>   
+   
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,23 +2,17 @@ import React from 'react'
 import './Style.css';
 function Navbar() {
   return (
-    <div  wrapper>
-        <nav>
+    <div className='nav-list'>
             <ul>
-                <li className='others2'><a href='#'>Gmail</a></li>
-                <li className='others2'><a href='#'>Images</a></li>
-                <li className='more-menu others2'>
-                    <a href='#'>More</a>
-                    <ul>
-                        <li><a href='#'>Google Apps</a></li>
-                        <li ><a href='#'>Google Meet</a></li>
-                        <li ><a href='#'>Google Drive</a></li>
-                        <li ><a href='#'>Google Calender</a></li>
-                    </ul>
+                <li className='nav-link'><a href='#'>Gmail</a></li>
+                <li className='nav-link'><a href='#'>Images</a></li>
+                <li className='more-menu'>
+                    <a href='#'>More
+                    </a>
+                    
                 </li>
-                <li className='others1' ><a href='#' className='others1'>Sign In</a></li>
+                <li className='sign-in-link' ><a href='#' className='sign-in-link'>Sign In</a></li>
             </ul>
-        </nav>
     </div>
   )
 }
